48929e8e9d0b8a4068a5fa966f1819c2c24aa1a3
ygladiensyah <ygladiensyah@gmail.com>
2019-07-26 21:54:43 +0700

    fix profile activity

--- app/src/main/java/com/nusantarian/fishcheck/Activity/ProfileActivity.java
+++ app/src/main/java/com/nusantarian/fishcheck/Activity/ProfileActivity.java
@@ -1,16 +1,87 @@
 package com.nusantarian.fishcheck.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.nusantarian.fishcheck.R;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_username, tv_name;
+    private String uid, name, username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        uid = Objects.requireNonNull(mUser).getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("Users").child("Profile Pictures");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        tv_username = findViewById(R.id.tv_username);
+        tv_name = findViewById(R.id.tv_name);
+        findViewById(R.id.btn_edit).setOnClickListener(this);
+        findViewById(R.id.img_profpic).setOnClickListener(this);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    name = Objects.requireNonNull(dataSnapshot.getValue()).toString();
+                    username = Objects.requireNonNull(dataSnapshot.getValue().toString());
+
+                    tv_name.setText(name);
+                    tv_username.setText(username);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_edit:
+                startActivity(new Intent(ProfileActivity.this, EditProfileActivity.class));
+                break;
+            case R.id.img_profpic:
+                break;
+        }
     }
 }
