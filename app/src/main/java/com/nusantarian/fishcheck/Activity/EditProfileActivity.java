2ae9ca65b71c41b8e7a159fad7b8a637cb0e2c23
ygladiensyah <ygladiensyah@gmail.com>
2019-07-26 22:59:36 +0700

    fix profile activity

--- app/src/main/java/com/nusantarian/fishcheck/Activity/EditProfileActivity.java
+++ app/src/main/java/com/nusantarian/fishcheck/Activity/EditProfileActivity.java
@@ -1,16 +1,38 @@
 package com.nusantarian.fishcheck.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.nusantarian.fishcheck.R;
 
+import java.util.Objects;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    private EditText et_email, et_username, et_name;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorage;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.pengaturan_profil);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
