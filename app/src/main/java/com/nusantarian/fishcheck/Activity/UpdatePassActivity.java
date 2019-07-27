5079b82349c0d82a7d63330757ad89d0d92e3948
ygladiensyah <ygladiensyah@gmail.com>
2019-07-27 15:32:37 +0700

    add update password activity

---
+++ app/src/main/java/com/nusantarian/fishcheck/Activity/UpdatePassActivity.java
@@ -0,0 +1,16 @@
+package com.nusantarian.fishcheck.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nusantarian.fishcheck.R;
+
+public class UpdatePassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_pass);
+    }
+}
